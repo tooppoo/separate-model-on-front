48afe192757031dd52db5f93843285ccbe927bde
philomagi <tooppoo56@gmail.com>
2021-02-02 02:06:34 +0900

    fix lint

--- packages/vue/src/views/cart/CartItem.vue
+++ packages/vue/src/views/cart/CartItem.vue
@@ -19,15 +19,9 @@
         </label>
       </div>
       <div class="cart-item__editor__price">計{{ cartItem.payment }}円</div>
-      <button @click="onRemove(cartItem)">
-        カートから取り除く
-      </button>
-      <button v-if="willPurchase" @click="onBuyLater(cartItem)">
-        後で買う
-      </button>
-      <button v-else @click="onBuyNow(cartItem)">
-        今すぐ買う
-      </button>
+      <button @click="onRemove(cartItem)">カートから取り除く</button>
+      <button v-if="willPurchase" @click="onBuyLater(cartItem)">後で買う</button>
+      <button v-else @click="onBuyNow(cartItem)">今すぐ買う</button>
     </div>
   </li>
 </template>
@@ -50,14 +44,14 @@ export default class CartItemView extends Vue {
     return this.cartItem.count;
   }
 
-  get itemImageClass(): object {
+  get itemImageClass(): Record<string, string> {
     const base = {
       "background-image": `url(${this.cartItem.picture.url})`,
       "background-position": "center",
       "background-repeat": "no-repeat",
       "background-size": "contain",
       width: "300px",
-      height: "300px"
+      height: "300px",
     };
 
     if (this.willPurchase) {
@@ -67,25 +61,25 @@ export default class CartItemView extends Vue {
     return {
       ...base,
       "background-color": "lightgray",
-      "background-blend-mode": "multiply"
+      "background-blend-mode": "multiply",
     };
   }
 
   @Emit("remove")
-  onRemove(_item: CartItem) {}
+  onRemove(_item: CartItem): void {}
 
   @Emit("buy-later")
-  onBuyLater(_item: CartItem) {}
+  onBuyLater(_item: CartItem): void {}
 
   @Emit("buy-now")
-  onBuyNow(_item: CartItem) {}
+  onBuyNow(_item: CartItem): void {}
 
-  onChangeCount(newCount: string) {
+  onChangeCount(newCount: string): void {
     this.emitChangeCount(this.cartItem, CountInCart.parse(newCount));
   }
 
   @Emit("change-count")
-  emitChangeCount(_item: CartItem, _newCount: CountInCart) {}
+  emitChangeCount(_item: CartItem, _newCount: CountInCart): void {}
 }
 </script>
 
